c29db5558f980ca0cf93622a120b779390636fee
Tomas Kadlcek <kadlcek.tomas@gmail.com>
2020-12-07 12:14:45 +0100

    added bs func

--- receptyscrap/receptyscrap.py
+++ receptyscrap/receptyscrap.py
@@ -12,10 +12,23 @@ def opendriver():
     driver = webdriver.Chrome("driver/chromedriver.exe")
     driver.get("https://www.alza.cz/wearables/18855068.htm#f&cst=0&cud=0&pg=1-50&prod=&sc=1022")
     sleep(5)
+    writetofile(driver)
     driver.quit()
-    return driver
 
 
+def writetofile(driver):
+    res = driver.page_source
+
+    soup = BeautifulSoup(res, "html.parser")
+
+    pretty_soup = soup.prettify()
+
+    html_file = open("html/alza_main.html", "wb")
+    html_file.write(pretty_soup.encode('utf-8'))
+    html_file.close()
+
+    print("5 seconds timeout")
+    sleep(5)
 
 
 opendriver()
