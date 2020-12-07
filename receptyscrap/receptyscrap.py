688e32f9885d6edd0e31879e23156aebea13e332
Tomas Kadlcek <kadlcek.tomas@gmail.com>
2020-12-07 12:07:46 +0100

    added mysql connector and setup.py

--- receptyscrap/receptyscrap.py
+++ receptyscrap/receptyscrap.py
@@ -5,11 +5,17 @@ import csv
 import codecs
 import re
 import datetime
+import mysql.connector
 
 
 def opendriver():
     driver = webdriver.Chrome("driver/chromedriver.exe")
     driver.get("https://www.alza.cz/wearables/18855068.htm#f&cst=0&cud=0&pg=1-50&prod=&sc=1022")
+    sleep(5)
+    driver.quit()
+    return driver
+
+
 
 
 opendriver()
